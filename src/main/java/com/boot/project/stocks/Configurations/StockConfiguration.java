cecf15fa7d5e5198058f0c8b29670e45d89417a5
Varun <varunbhardwaj79@gmail.com>
2019-09-19 19:18:11 +0530

    Stock Config update Spring Boot Project Commit

--- src/main/java/com/boot/project/stocks/Configurations/StockConfiguration.java
+++ src/main/java/com/boot/project/stocks/Configurations/StockConfiguration.java
@@ -1,12 +1,21 @@
 package com.boot.project.stocks.Configurations;
 
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import javax.net.ssl.SSLException;
 import java.util.HashMap;
 import java.util.Map;
+
 
 
 @Configuration
@@ -14,13 +23,22 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "my")
 public class StockConfiguration {
 
-    private final Map<String, String> companyMap = new HashMap<> ();
+    private final Map<String, String> companyNames = new HashMap<>();
 
-    public Map<String, String> getCompanyMap () {
-        return companyMap;
+    public Map<String, String> getCompanyNames (){
+        return companyNames;
     }
 
 
+    @Bean
+    public WebClient createWebClient () throws SSLException{
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager( InsecureTrustManagerFactory.INSTANCE )
+                .build();
+        HttpClient httpConnector = HttpClient.create().secure( sslContextSpec -> sslContextSpec.sslContext( sslContext ) );
+        return WebClient.builder().clientConnector( new ReactorClientHttpConnector( httpConnector ) ).build();
+    }
 }
 
 
