78c1f8c0ce56290495220faf1018064fa8b22628
Varun <varunbhardwaj79@gmail.com>
2019-09-19 19:22:05 +0530

    Stock update Spring Boot Project Commit

--- src/main/java/com/boot/project/stocks/StockBootApplication.java
+++ src/main/java/com/boot/project/stocks/StockBootApplication.java
@@ -2,14 +2,10 @@ package com.boot.project.stocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableScheduling
-@PropertySource("classpath:messaging_queues.properties")
-@ConfigurationProperties(prefix = "app")
+@EnableAsync
 public class StockBootApplication {
 
 
