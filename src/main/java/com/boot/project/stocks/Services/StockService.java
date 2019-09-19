505b218c8027735ab92d261eafad555ba489e8bc
Varun <varunbhardwaj79@gmail.com>
2019-09-19 22:53:33 +0530

    Stock Service update with yahoo finance api  Spring Boot Project Commit

--- src/main/java/com/boot/project/stocks/Services/StockService.java
+++ src/main/java/com/boot/project/stocks/Services/StockService.java
@@ -9,10 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import javax.annotation.PostConstruct;
 import java.util.LinkedHashMap;
@@ -27,65 +27,66 @@ public class StockService {
     @Autowired
     private WebClient webClient;
 
-    @Autowired
     private StockConfiguration stockConfiguration;
-
     private Map<String, String> companyTickerMap;
-
-
     @Value("${iex.api.key}")
     private String IEX_API_KEY;
-
-    @Value("${alpha.vantage.api.key}")
-    private String ALPHA_API_KEY;
-
-    @Value("${alpha.vantage.url}")
-    private String ALPHA_URL;
-
+    @Value("${yahoo.finance.url}")
+    private String YAHOO_URL;
     @Value("${iex.cloud.url}")
     private String IEX_URL;
 
+    public StockService (StockConfiguration stockConfiguration){
+        this.stockConfiguration = stockConfiguration;
+    }
+
     @PostConstruct
-    @Async
     public void saveAllTickerSymbols (){
         companyTickerMap = new LinkedHashMap<>();
         for (String companyName : stockConfiguration.getCompanyNames().keySet()) {
-            String tickerSymbol = getStockSymbol( companyName );
-            companyTickerMap.put( companyName, getStockSymbol( tickerSymbol ) );
-            log.info( "Generated Ticker Symbol : {}", tickerSymbol );
-        }
-    }
-
-
-    private String getStockSymbol (String stock){
-        String ticker_symbol = null;
-        String result = webClient.get().uri( ALPHA_URL+stock+"&apikey="+ALPHA_API_KEY )
-                .retrieve().bodyToMono( String.class ).
-                        block().replaceAll( "\n", "" );
-        JSONObject jsonObject = new JSONObject( result );
-        try {
-            JSONArray jsonArray = jsonObject.getJSONArray( "bestMatches" );
-            if(jsonArray.length() != 0){
-                ticker_symbol = String.valueOf( jsonArray.getJSONObject( 0 ).get( "1. symbol" ) );
+            String tickerSymbol = generateTickerFromJSON( companyName );
+            if(tickerSymbol != null){
+                companyTickerMap.put( companyName, tickerSymbol );
+                log.info( "Generated Ticker Symbol : {}", tickerSymbol );
             }
-        } catch (JSONException e) {
-            log.info( "Unable to generate Ticker Symmbol : {}", stock+e.getLocalizedMessage() );
         }
-        return ticker_symbol;
     }
+
+
+    private String generateTickerFromJSON (String stock){
+
+        String url = YAHOO_URL+stock+"&region=1&lang=en";
+        String tickerSymbol = null;
+        try {
+            String json = webClient.get().uri( url ).retrieve().bodyToMono( String.class ).block();
+            JSONObject jsonObject = new JSONObject( json );
+            int objectIncrementer = 0;
+            JSONArray jsonArray = jsonObject.getJSONObject( "ResultSet" ).getJSONArray( "Result" );
+            if(jsonArray.length() != 0){
+                tickerSymbol = String.valueOf( jsonArray.getJSONObject( objectIncrementer ).get( "symbol" ) );
+                while (tickerSymbol.contains( "." )) {
+                    tickerSymbol = String.valueOf( jsonArray.getJSONObject( ++objectIncrementer ).get( "symbol" ) );
+                }
+            }
+        } catch (RestClientException | JSONException e) {
+            log.info( "Unable to generate ticker symbol for company name  {}", stock );
+        }
+        return tickerSymbol;
+    }
+
 
     public StockDetails fetchStockInfo (String stockName){
-        StockDetails stockDetails = null;
         String stockSymbol = companyTickerMap != null ? companyTickerMap.get( stockName ) : null;
+        StockDetails stockDetails = null;
         try {
             if (stockSymbol != null) {
                 String restUrl = IEX_URL+stockSymbol+"/quote?token="+IEX_API_KEY;
                 stockDetails = webClient.get().uri( restUrl ).retrieve().bodyToMono( StockDetails.class ).block();
-                stockDetails.setCompanyName ( stockName );
                 log.info( stockDetails.toString() );
+                return stockDetails;
             }
-        } catch (RestClientException res) {
-            System.out.println( res.getLocalizedMessage()+res.getMessage() );
+        } catch (RestClientException | WebClientResponseException res) {
+            System.out.println( "Response error exception by company name : "+stockName+" "+res.getMessage() );
         }
         return stockDetails;
     }
